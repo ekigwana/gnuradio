01a000e6c0a4fba544d6e697daf5be2bc0be6f44
Edward Kigwana <ekigwana@gmail.com>
2020-07-23 05:08:46 +0000

    iio: Enable pybind11 bindings
    Signed-off-by: Edward Kigwana <ekigwana@gmail.com>

--- gr-iio/python/iio/__init__.py
+++ gr-iio/python/iio/__init__.py
@@ -28,5 +28,8 @@ from __future__ import unicode_literals
 import os
 
 try:
+    from .iio_python import *
 except ImportError:
     dirname, filename = os.path.split(os.path.abspath(__file__))
+    __path__.append(os.path.join(dirname, "bindings"))
+    from .iio_python import *
